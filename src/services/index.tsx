60c98a30964182af8fa4044a2dcdc8978e9823f6
araz <araz944memmedov@gmail.com>
2024-11-11 21:29:46 +0400

    Added refreshAccessToken() function

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -4,6 +4,48 @@ import { STORAGE } from "../utils/storage";
 
 const getToken = () => STORAGE({ type: EStorage.SESSION, key: EStorageKeys.BUDGET_APP_CREDS }).get()?.access_token;
 
+// Utility function to check token expiration
+const isTokenExpired = (token) => {
+  const tokenParts = token.split('.');
+  if (tokenParts.length === 3) {
+    const payload = JSON.parse(atob(tokenParts[1]));
+    const exp = payload.exp * 1000;
+    return Date.now() > exp;
+  }
+  return true;
+};
+
+// Refresh the access token if it has expired
+const refreshAccessToken = async () => {
+  try {
+    const refreshToken = STORAGE({ type: EStorage.SESSION, key: EStorageKeys.BUDGET_APP_CREDS }).get()?.refresh_token;
+    const response = await axios.post(
+      'http://localhost:8081/api/v1/refresh',
+      refreshToken
+    );
+    
+    const { access_token } = response.data;
+
+    // Update the storage with the new access token
+    const storedData = STORAGE({ type: EStorage.SESSION, key: EStorageKeys.BUDGET_APP_CREDS }).get();
+    STORAGE({ type: EStorage.SESSION, key: EStorageKeys.BUDGET_APP_CREDS }).set({
+      ...storedData,
+      access_token,
+    });
+    
+    return access_token;
+  } catch (error) {
+    if (error.response?.data?.error === 'Refresh token expired') {
+      STORAGE({ type: EStorage.SESSION, key: EStorageKeys.BUDGET_APP_CREDS }).clear();
+      window.history.pushState({}, '', '/');
+      window.dispatchEvent(new PopStateEvent('popstate'));
+    } else {
+      console.error('Failed to refresh access token:', error);
+    }
+    return null;
+  }
+};
+
 const instance = axios.create({
   headers: {
     'Content-Type': 'application/json'
@@ -11,12 +53,24 @@ const instance = axios.create({
 });
 
 // Add interceptor to include the token only if it exists
-instance.interceptors.request.use((config) => {
-  const token = getToken();
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+instance.interceptors.request.use(async (config) => {
+  let token = getToken();
+  
+  // If the token exists and it's expired, refresh it
+  if (token && isTokenExpired(token)) {
+    token = await refreshAccessToken();
+    if (!token) {
+      return Promise.reject(new Error('Authentication required'));
+    }
   }
+  
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+
   return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export const REQUEST = {
