79d4c3ce6e6d9fda3a620352b97f1915ec949a0d
araz <araz944memmedov@gmail.com>
2024-11-14 22:51:47 +0400

    Added sections

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
-import { Box, Text, VStack } from '@chakra-ui/react';
-import useBudgetTrackerStore from '../store';
+import { Box, Flex, Text, VStack } from '@chakra-ui/react';
+import { ThemeProvider, createTheme } from "@mui/material";
 import useGetAllCategories from "../hooks/useGetAllCategories";
+import Categories from "./Categories";
+import CustomPieChart from "./CustomPieChart";
+
+const theme = createTheme();
 
 const BudgetTracker: React.FC = () => {
   const { getAllCategories } = useGetAllCategories();
-  const { categories, budgets, transactions } = useBudgetTrackerStore();
 
   useEffect(() => {
     getAllCategories();
@@ -13,24 +16,61 @@ const BudgetTracker: React.FC = () => {
 
   return (
     <VStack spacing={4}>
-      <Box>
-        <Text fontSize="xl" fontWeight="bold">Categories</Text>
-        {categories.map(category => (
-          <Text key={category.category_id}>{category.name}</Text>
-        ))}
-      </Box>
-      <Box>
-        <Text fontSize="xl" fontWeight="bold">Budgets</Text>
-        {budgets.map(budget => (
-          <Text key={budget.budget_id}>Amount: {budget.amount} for {budget.year}-{budget.month}</Text>
-        ))}
-      </Box>
-      <Box>
-        <Text fontSize="xl" fontWeight="bold">Transactions</Text>
-        {transactions.map(transaction => (
-          <Text key={transaction.transaction_id}>Transaction: {transaction.amount} on {transaction.date}</Text>
-        ))}
-      </Box>
+      <Flex gap={4} justify="center" width="100%" height="48vh">
+        <Box
+          flex="1"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          minW="200px"
+          overflow="auto"
+          bgColor="lightgoldenrodyellow"
+        >
+          <Categories />
+        </Box>
+        <ThemeProvider theme={theme}>
+          <Box
+            flex="1"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            minW="200px"
+            bgColor="#F1F1F1"
+            overflow="auto"
+          >
+            <CustomPieChart />
+          </Box>
+        </ThemeProvider>
+      </Flex>
+
+      <Flex gap={4} justify="center" width="100%" height="48vh">
+        <Box
+          flex="1"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          minW="200px"
+          overflow="auto"
+          bgColor="lightgray"
+        >
+          <Text fontSize="xl" fontWeight="bold">
+            Budgets
+          </Text>
+        </Box>
+        <Box
+          flex="1"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          minW="200px"
+          overflow="auto"
+          bgColor="lightskyblue"
+        >
+          <Text fontSize="xl" fontWeight="bold">
+            Transactions
+          </Text>
+        </Box>
+      </Flex>
     </VStack>
   );
 };
