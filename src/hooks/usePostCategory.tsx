368bcfd09a9adf42424a4b2a10e2604eb0177a44
araz <araz944memmedov@gmail.com>
2024-11-16 22:53:18 +0400

    Added changes into onSuccess() method

--- src/hooks/usePostCategory.tsx
+++ src/hooks/usePostCategory.tsx
@@ -4,15 +4,19 @@ import { ENDPOINTS } from "../services/servicesList";
 import useBudgetTrackerStore from "../store";
 
 const usePostCategory = () => {
-  const { categoryObj } = useBudgetTrackerStore();
+  const { categoryObj, categories, setCategories, setNewCategoryStatus } = useBudgetTrackerStore();
 
   const { mutate: saveNewCategory } = useMutation({
     mutationFn: () =>
       REQUEST.post(ENDPOINTS.CATEGORIES.post(), categoryObj),
-    onSuccess: (response) => {
-      console.log(response);
+    onSuccess: ({data}) => {
+      // console.log(data);
 
-      // setCategories(response);
+      setNewCategoryStatus(null);
+      setCategories([
+        ...categories,
+        data,
+      ]);
     },
     onError: (error) => {
       console.error('Error fetching data:', error);
