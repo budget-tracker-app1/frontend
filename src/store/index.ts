66f2c195a5b289e1afd7bde4255e57b081aa4baf
araz <araz944memmedov@gmail.com>
2024-11-16 22:45:16 +0400

    Added new state into store file

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,10 +6,12 @@ interface BudgetTrackerState {
   categories: any[];
   newCategoryStatus: CategoryType | null;
   categoryObj: ICategory;
+  exactCategoryId: number | null;
 
   setCategories: (data: any[]) => void;
   setNewCategoryStatus: (data: CategoryType | null) => void;
   setCategoryObj: (data: ICategory) => void;
+  setExactCategoryId: (data: number | null) => void;
 }
 
 const useBudgetTrackerStore = create<BudgetTrackerState>((set) => ({
@@ -20,10 +22,12 @@ const useBudgetTrackerStore = create<BudgetTrackerState>((set) => ({
     name: "",
     color: null,
   },
+  exactCategoryId: null,
 
   setCategories: (data) => set(() => ({ categories: data })),
   setNewCategoryStatus: (data) => set(() => ({ newCategoryStatus: data })),
   setCategoryObj: (data) => set(() => ({ categoryObj: data })),
+  setExactCategoryId: (data) => set(() => ({ exactCategoryId: data })),
 }));
 
 export default useBudgetTrackerStore;
