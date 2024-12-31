7d31067543300511b50a0bd9b05d6a67afbdf1e7
araz <araz944memmedov@gmail.com>
2024-12-31 19:44:23 +0400

    Add logout

--- src/components/Budgets/index.tsx
+++ src/components/Budgets/index.tsx
@@ -1,9 +1,15 @@
-import { HStack, Text } from "@chakra-ui/react"
+import { Box, Flex, HStack, IconButton, Text } from "@chakra-ui/react"
 import React from 'react'
 import MyAccounts from "./MyAccounts"
 import Transactions from "../Transactions"
+import { ArrowBackIcon } from "@chakra-ui/icons";
 
 const Budgets = () => {
+  const handleLogout = () => {
+    sessionStorage.clear();
+    window.location.href = '/';
+  };
+
   return (
     <>
       <Text fontSize="xl" fontWeight="bold">Budgets</Text>
@@ -18,6 +24,39 @@ const Budgets = () => {
         <MyAccounts />
         <Transactions />
       </HStack>
+      <Box
+        position="fixed"
+        bottom="50px"
+        left="40px"
+        bgColor="white"
+        padding="10px 20px"
+        borderRadius={50}
+        cursor="pointer"
+        boxShadow="lg"
+        _hover={{
+          bgColor: "gray.200",
+          boxShadow: "lg",
+          transform: "scale(1.02)",
+          transition: "all 0.2s ease-in-out",
+        }}
+      >
+        <Flex
+          alignItems="center"
+          gap="10px"
+          onClick={handleLogout}
+        >
+          <IconButton
+            aria-label="Logout"
+            icon={<ArrowBackIcon />}
+            size="lg"
+            colorScheme="red"
+            borderRadius="full"
+          />
+          <Text fontSize="20px" fontWeight="bold" color="black.500">
+            Logout
+          </Text>
+        </Flex>
+      </Box>
     </>
   )
 }
