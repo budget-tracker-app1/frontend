974bceb7ced22afc4b0ddd3f0cc628ec30a3fa49
araz <araz944memmedov@gmail.com>
2025-01-01 16:02:34 +0400

    Add modal and tooltip overlays

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Flex, VStack } from '@chakra-ui/react';
 import useGetAllCategories from "../hooks/http/useGetAllCategories";
 import Categories from "./Categories";
@@ -6,10 +6,17 @@ import Budgets from "./Budgets";
 import useGetAllTransactions from "../hooks/http/useGetAllTransactions";
 import History from "./History";
 import Finances from "./Finances";
+import TooltipOverlay from "./TooltipOverlay";
+import { steps } from "../data/tourSteps";
+import Tutorial from "./Modal/Tutorial";
+import useBudgetTrackerStore from "../store";
 
 const BudgetTracker: React.FC = () => {
   const { getAllCategories } = useGetAllCategories();
   const { getAllTransactions } = useGetAllTransactions();
+  const { startTutorial } = useBudgetTrackerStore()
+
+  const [currentStep, setCurrentStep] = useState(0);
 
   useEffect(() => {
     getAllCategories();
@@ -19,58 +26,83 @@ const BudgetTracker: React.FC = () => {
     getAllTransactions();
   }, [getAllTransactions]);
 
-  return (
-    <VStack spacing={4}>
-      <Flex gap={4} justify="center" width="100%" height="48vh">
-        <Box
-          flex="1"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          minW="200px"
-          overflow="auto"
-          bgColor="lightgoldenrodyellow"
-        >
-          <Categories />
-        </Box>
-        <Box
-          flex="1"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          minW="200px"
-          overflow="auto"
-          bgColor="#F1F1F1"
-        >
-          <Finances />
-        </Box>
-      </Flex>
+  const handleNext = () => {
+    if (currentStep < steps.length - 1) {
+      setCurrentStep((prev) => prev + 1);
+    } else {
+      setCurrentStep(steps.length);
+    }
+  };
 
-      <Flex gap={4} justify="center" width="100%" height="48vh">
-        <Box
-          flex="1"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          minW="200px"
-          overflow="auto"
-          bgColor="lightgray"
-        >
-          <Budgets />
-        </Box>
-        <Box
-          flex="1"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          minW="200px"
-          overflow="auto"
-          bgColor="lightskyblue"
-        >
-          <History />
-        </Box>
-      </Flex>
-    </VStack>
+  const handleSkip = () => {
+    setCurrentStep(steps.length);
+  };
+
+  return (
+    <>
+      <VStack spacing={4}>
+        <Flex gap={4} justify="center" width="100%" height="48vh">
+          <Box
+            flex="1"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            minW="200px"
+            overflow="auto"
+            bgColor="lightgoldenrodyellow"
+          >
+            <Categories />
+          </Box>
+          <Box
+            flex="1"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            minW="200px"
+            overflow="auto"
+            bgColor="#F1F1F1"
+          >
+            <Finances />
+          </Box>
+        </Flex>
+
+        <Flex gap={4} justify="center" width="100%" height="48vh">
+          <Box
+            flex="1"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            minW="200px"
+            overflow="auto"
+            bgColor="lightgray"
+          >
+            <Budgets />
+          </Box>
+          <Box
+            flex="1"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            minW="200px"
+            overflow="auto"
+            bgColor="lightskyblue"
+          >
+            <History />
+          </Box>
+        </Flex>
+      </VStack>
+
+      {startTutorial && currentStep < steps.length && (
+        <TooltipOverlay
+          currentStep={currentStep}
+          steps={steps}
+          onNext={handleNext}
+          onSkip={handleSkip}
+        />
+      )}
+
+      {<Tutorial />}
+    </>
   );
 };
 
--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,6 +1,7 @@
 import { HStack, Text } from "@chakra-ui/react"
 import React from 'react'
 import CategoryColumn from "./CategoryColumn";
+import ImportantInformation from "../Modal/ImportantInformation";
 
 export enum CategoryType {
   ACCOUNT = "ACCOUNT",
@@ -13,10 +14,11 @@ const Categories = () => {
     <>
       <Text fontSize="xl" fontWeight="bold">Categories</Text>
       <HStack spacing={8} width="100%" maxW="900px" margin="0 auto" alignItems="flex-start">
-        <CategoryColumn type={CategoryType.ACCOUNT} />
-        <CategoryColumn type={CategoryType.INCOME} />
-        <CategoryColumn type={CategoryType.EXPENSE} />
+        <CategoryColumn type={CategoryType.ACCOUNT} id="add-category-button" />
+        <CategoryColumn type={CategoryType.INCOME} id="category-input" />
+        <CategoryColumn type={CategoryType.EXPENSE} id="save-category-button" />
       </HStack>
+      <ImportantInformation />
     </>
   )
 }
