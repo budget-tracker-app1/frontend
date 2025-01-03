00746138426a0c7f59a5989e96e34d2682ed6194
araz <araz944memmedov@gmail.com>
2025-01-03 20:29:17 +0400

    Add new states into store

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,29 +1,59 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 import { ENDPOINTS } from "../services/servicesList";
 
 interface AuthState {
-    register: (username: string, password: string) => Promise<void>;
+  isLoginPage: boolean;
+  setIsLoginPage: (value: boolean) => void;
+  register: (
+    username: string,
+    password: string,
+    toast: any,
+    setIsLoading: (value: boolean) => void
+  ) => Promise<void>;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
-    register: async (username, password) => {
-        try {
-            const response = await fetch(ENDPOINTS.REGISTER(), {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username, password }),
-            });
-            
-            if (!response.ok) {
-                throw new Error('Registration failed');
-            }
-            
-            alert('Registration successful!');
-        } catch (error) {
-            console.error(error);
-            alert('Registration failed, please try again');
-        }
-    },
+  isLoginPage: true,
+  setIsLoginPage: (value) => set({ isLoginPage: value }),
+  register: async (username, password, toast, setIsLoading) => {
+    try {
+      setIsLoading(true);
+
+      const response = await fetch(ENDPOINTS.REGISTER(), {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Server error. Registration failed. Please try again.");
+      }
+
+      if (!toast.isActive("error-toast")) {
+        toast({
+          title: "Success.",
+          description: "Registration successful! Please log in.",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+      set({ isLoginPage: true });
+    } catch (error: any) {
+      if (!toast.isActive("error-toast")) {
+        toast({
+          id: "error-toast",
+          title: "Error.",
+          description: error.message || "Registration failed, please try again.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  },
 }));
