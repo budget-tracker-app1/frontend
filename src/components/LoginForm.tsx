c54ceb483021c8c556703364d834c6246978c6e1
araz <araz944memmedov@gmail.com>
2025-01-03 20:29:36 +0400

    Add loading for button

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Box,
   Button,
+  Flex,
   FormControl,
   FormLabel,
   Heading,
   Input,
   Stack,
   useToast,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 import { useMutation } from "react-query";
 import { REQUEST } from "../services";
 import { ENDPOINTS } from "../services/servicesList";
@@ -17,78 +18,131 @@ import { EStorage, EStorageKeys } from "../models";
 import { useNavigate } from "react-router-dom";
 
 const LoginForm: React.FC = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
   const toast = useToast();
+  const TOAST_ID = "error-toast";
 
   const { mutate } = useMutation({
     mutationFn: () =>
-      REQUEST.post(ENDPOINTS.LOGIN(), {username, password}, {
-        headers: { Authorization: '' }
-      }),
+      REQUEST.post(
+        ENDPOINTS.LOGIN(),
+        { username, password },
+        {
+          headers: { Authorization: "" },
+        }
+      ),
     onSuccess: (response) => {
       // console.log(response);
 
+      setIsLoading(false);
       const creds = response.data;
-      STORAGE({ type: EStorage.SESSION, key: EStorageKeys.BUDGET_APP_CREDS }).set(creds);
-      toast({
-        title: "Logged in.",
-        description: "You have successfully logged in.",
-        status: "success",
-        duration: 3000,
-        isClosable: true,
-      });
-      setUsername('');
-      setPassword('');
+      STORAGE({
+        type: EStorage.SESSION,
+        key: EStorageKeys.BUDGET_APP_CREDS,
+      }).set(creds);
+      if (!toast.isActive(TOAST_ID)) {
+        toast({
+          id: TOAST_ID,
+          title: "Logged in.",
+          description: "You have successfully logged in.",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+      setUsername("");
+      setPassword("");
       navigate("/dashboard");
     },
     onError: (error) => {
-      console.error('Error fetching data:', error);
-      toast({
-        title: "Login error.",
-        description: "Given wrong credentials.",
-        status: "error",
-        duration: 3000,
-        isClosable: true,
-      });
+      console.error("Error fetching data:", error);
+      setIsLoading(false);
+      if (!toast.isActive(TOAST_ID)) {
+        toast({
+          id: TOAST_ID,
+          title: "Login error.",
+          description: "Given wrong credentials.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
     },
   });
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
+    setIsLoading(true);
     mutate();
   };
 
   return (
-    <Box width="400px" mx="auto" mt="100px" p={5} borderWidth={1} borderRadius="lg">
-        <Heading mb="6" size="lg" textAlign="center">Login</Heading>
-      <form onSubmit={handleSubmit}>
-        <Stack spacing={4}>
-          <FormControl isRequired>
-            <FormLabel htmlFor="username">Username</FormLabel>
-            <Input
-              id="username"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <Input
-              id="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </FormControl>
-          <Button type="submit" colorScheme="teal">
-            Login
-          </Button>
-        </Stack>
-      </form>
-    </Box>
+    <Flex height="100vh" justifyContent="center" alignItems="center">
+      <Box
+        width="400px"
+        mx="auto"
+        p="6"
+        bgColor="#e6e6e6"
+        boxShadow="2xl"
+        borderRadius="md"
+        borderWidth="1px"
+      >
+        <Heading mb="6" size="lg" textAlign="center">
+          Login
+        </Heading>
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={4}>
+            <FormControl isRequired>
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input
+                id="username"
+                type="text"
+                value={username}
+                maxLength={32}
+                bgColor="#FFFFFF"
+                boxShadow="md"
+                _focus={{
+                  boxShadow: "lg",
+                  outline: "none",
+                  borderColor: "transparent",
+                }}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <Input
+                id="password"
+                type="password"
+                value={password}
+                maxLength={32}
+                bgColor="#FFFFFF"
+                boxShadow="md"
+                _focus={{
+                  boxShadow: "lg",
+                  outline: "none",
+                  borderColor: "transparent",
+                }}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </FormControl>
+            <Button
+              colorScheme="teal"
+              type="submit"
+              width="full"
+              boxShadow="lg"
+              mt="30px"
+              isLoading={isLoading}
+            >
+              Login
+            </Button>
+          </Stack>
+        </form>
+      </Box>
+    </Flex>
   );
 };
 
--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,60 +1,186 @@
-import React, { useState } from 'react';
-import { Box, Button, Input, FormControl, FormLabel, Heading, Stack } from '@chakra-ui/react';
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Input,
+  FormControl,
+  FormLabel,
+  Heading,
+  Stack,
+  useToast,
+  Flex,
+} from "@chakra-ui/react";
 import { useAuthStore } from "../store/authStore";
 
 const RegisterForm: React.FC = () => {
-    const register = useAuthStore((state) => state.register);
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+  const register = useAuthStore((state) => state.register);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+  const TOAST_ID = "error-toast";
 
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-        if (password !== confirmPassword) {
-            alert("Passwords do not match");
-            return;
-        }
-        
-        await register(username, password);
-    };
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
 
-    return (
-        <Box maxWidth="400px" mx="auto" mt="8" p="6" boxShadow="lg" borderRadius="md" borderWidth="1px">
-            <Heading mb="6" size="lg" textAlign="center">Register</Heading>
-            <form onSubmit={handleSubmit}>
-                <Stack spacing={4}>
-                    <FormControl id="username" isRequired>
-                        <FormLabel>Username</FormLabel>
-                        <Input 
-                            type="text" 
-                            value={username} 
-                            onChange={(e) => setUsername(e.target.value)} 
-                        />
-                    </FormControl>
-                    
-                    <FormControl id="password" isRequired>
-                        <FormLabel>Password</FormLabel>
-                        <Input 
-                            type="password" 
-                            value={password} 
-                            onChange={(e) => setPassword(e.target.value)} 
-                        />
-                    </FormControl>
-                    
-                    <FormControl id="confirm-password" isRequired>
-                        <FormLabel>Confirm Password</FormLabel>
-                        <Input 
-                            type="password" 
-                            value={confirmPassword} 
-                            onChange={(e) => setConfirmPassword(e.target.value)} 
-                        />
-                    </FormControl>
-                    
-                    <Button colorScheme="teal" type="submit" width="full">Register</Button>
-                </Stack>
-            </form>
-        </Box>
-    );
+    const validCharactersRegex = /^[a-zA-Z0-9!@#$%^&*_-]+$/;
+
+    if (username.length < 3) {
+      if (!toast.isActive(TOAST_ID)) {
+        toast({
+          id: TOAST_ID,
+          title: "Error.",
+          description: "Username must be at least 3 characters long.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+      return;
+    }
+
+    if (!validCharactersRegex.test(username)) {
+      if (!toast.isActive(TOAST_ID)) {
+        toast({
+          id: TOAST_ID,
+          title: "Error.",
+          description: "Username contains unsupported characters.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+      return;
+    }
+
+    if (password.length < 3) {
+      if (!toast.isActive(TOAST_ID)) {
+        toast({
+          id: TOAST_ID,
+          title: "Error.",
+          description: "Password must be at least 3 characters long.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+      return;
+    }
+
+    if (!validCharactersRegex.test(password)) {
+      if (!toast.isActive(TOAST_ID)) {
+        toast({
+          id: TOAST_ID,
+          title: "Error.",
+          description: "Password contains unsupported characters.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      if (!toast.isActive(TOAST_ID)) {
+        toast({
+          id: TOAST_ID,
+          title: "Error.",
+          description: "Passwords do not match.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+      return;
+    }
+
+    await register(username, password, toast, setIsLoading);
+  };
+
+  return (
+    <Flex height="100vh" justifyContent="center" alignItems="center">
+      <Box
+        width="400px"
+        mx="auto"
+        p="6"
+        bgColor="#e6e6e6"
+        boxShadow="2xl"
+        borderRadius="md"
+        borderWidth="1px"
+      >
+        <Heading mb="6" size="lg" textAlign="center">
+          Register
+        </Heading>
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={4}>
+            <FormControl id="username" isRequired>
+              <FormLabel>Username</FormLabel>
+              <Input
+                type="text"
+                value={username}
+                maxLength={32}
+                bgColor="#FFFFFF"
+                boxShadow="md"
+                _focus={{
+                  boxShadow: "lg",
+                  outline: "none",
+                  borderColor: "transparent",
+                }}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </FormControl>
+
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <Input
+                type="password"
+                value={password}
+                maxLength={32}
+                bgColor="#FFFFFF"
+                boxShadow="md"
+                _focus={{
+                  boxShadow: "lg",
+                  outline: "none",
+                  borderColor: "transparent",
+                }}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </FormControl>
+
+            <FormControl id="confirm-password" isRequired>
+              <FormLabel>Confirm Password</FormLabel>
+              <Input
+                type="password"
+                value={confirmPassword}
+                maxLength={32}
+                bgColor="#FFFFFF"
+                boxShadow="md"
+                _focus={{
+                  boxShadow: "lg",
+                  outline: "none",
+                  borderColor: "transparent",
+                }}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </FormControl>
+
+            <Button
+              colorScheme="teal"
+              type="submit"
+              width="full"
+              boxShadow="lg"
+              mt="30px"
+              isLoading={isLoading}
+            >
+              Register
+            </Button>
+          </Stack>
+        </form>
+      </Box>
+    </Flex>
+  );
 };
 
 export default RegisterForm;
